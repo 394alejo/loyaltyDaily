d7472158d5a31a569e21450cc3f7e16d8dd8bfcf
Alejandro Bedoya Solano <alejandrobedoyasolano@Mac.lan>
2026-06-27 22:49:17 -0500

    fix: remove staticwebapp.config.json that was causing unknown exception

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import path from 'path';
-import fs from 'fs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-const copy404Plugin = {
-  name: 'copy-index-to-404',
-  closeBundle() {
-    if (fs.existsSync('dist/index.html')) {
-      fs.copyFileSync('dist/index.html', 'dist/404.html');
-    }
-  }
-};
 
 export default defineConfig(({ mode }) => {
     return {
@@ -18,7 +8,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react(), copy404Plugin],
+      plugins: [react()],
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
